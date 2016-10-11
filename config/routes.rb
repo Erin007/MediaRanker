c57547f6338a45fa281328002134d49b53e381fe
Erin Bond <erin@erinebond.com>
2016-10-11 13:04:31 -0700

    Generated controller for movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+
+  get 'movies/show'
+
+  get 'movies/eidt'
+
+  get 'movies/update'
+
+  get 'movies/new'
+
+  get 'movies/destroy'
+
+  get 'movies/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
