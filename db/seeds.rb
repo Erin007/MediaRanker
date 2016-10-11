aa5436e9fb61dd8ad25b5e9b156f693d7b8da9fe
Erin Bond <erin@erinebond.com>
2016-10-11 15:35:37 -0700

    Seeded database with movie information

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,10 @@ CSV.foreach('seeds_csvs/books.csv', :headers => true) do |csv_obj|
   Book.create( name: csv_obj['name'], author: csv_obj['author'], description: csv_obj['description'], votes: csv_obj['votes'].to_i)
 end
 
+CSV.foreach('seeds_csvs/movies.csv', :headers => true) do |csv_obj|
+  Movie.create( name: csv_obj['name'], director: csv_obj['director'], description: csv_obj['description'], votes: csv_obj['votes'].to_i)
+end
+
 
 
 
