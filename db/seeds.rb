4ab001db0270779bbba37f779113b765a61bcf98
Erin Bond <erin@erinebond.com>
2016-10-11 13:56:49 -0700

    Seeded database with book csv info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+require "csv"
+
+CSV.foreach('seeds_csvs/books.csv', :headers => true) do |csv_obj|
+  Book.create( name: csv_obj['name'], author: csv_obj['author'], description: csv_obj['description'], votes: csv_obj['votes'].to_i)
+end
+
+
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
