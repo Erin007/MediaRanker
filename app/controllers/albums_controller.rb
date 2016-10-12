42babdf0731f3eecf726d5dce5970022090f35b3
Erin Bond <erin@erinebond.com>
2016-10-11 19:01:18 -0700

    Albums controller methods

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,22 +1,45 @@
 class AlbumsController < ApplicationController
+  def album
+    @album ||= Album.find(params[:id].to_i)
+  end
+
   def index
+    @albums = Album.order(:votes).reverse_order
   end
 
   def show
+    album
   end
 
-  def eidt
+  def edit
+    album
   end
 
   def update
+    album.update_attributes(album_params)
+    redirect_to album_path
   end
 
   def new
+    @album = Album.new
   end
 
   def destroy
+    album.destroy
+    redirect_to :albums
   end
 
   def create
+    @album = Album.new(name:params[:album][:name],
+                     artist:params[:album][:artist],
+                     description:params[:album][:description],
+                     votes: 0)
+   @album.save
+   redirect_to :albums
+  end
+
+private
+  def album_params
+    params.require(:album).permit( :name, :artist, :description, :votes)
   end
 end
