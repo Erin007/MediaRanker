bc2b3d6d49707b79253885d1d92ba86cdfdc5e26
Erin Bond <erin@erinebond.com>
2016-10-12 13:10:25 -0700

    Validations for book, movie and album

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ActiveRecord::Base
+  validates :name, presence: true
+  validates :author, presence: true
+  validates :description, presence: true
+  validates :votes, numericality: { only_integer: true }
 end
