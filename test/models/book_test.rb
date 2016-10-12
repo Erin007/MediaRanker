a2e435d010e1f3c948fd7233843b41c5a6390acd
Erin Bond <erin@erinebond.com>
2016-10-12 13:36:05 -0700

    Testing retrieval of attributes for book model

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
   test "A book must have a name, an author, and a description" do
     fake_book = Book.new
@@ -16,11 +13,19 @@ class BookTest < ActiveSupport::TestCase
     assert fake_book_name_author.invalid? "A book must have a name, an author, and a description"
 
     fake_book_name_author_description = Book.new ({name: "A Wrinkle in Time", author: "Madeline L'Engle", description: "A young girl must traverse the multiverse to save her quirky brother and father.", votes: 0})
-    puts fake_book_name_author_description.name
-    puts fake_book_name_author_description.author
-    puts fake_book_name_author_description.description
-    fake_book_name_author_description.save
     assert fake_book_name_author_description.valid?
   end
 
+  test "A books vote attirbute must be a number." do
+    fake_book_nonnum_vote = Book.new ({name: "A Wrinkle in Time", author: "Madeline L'Engle", description: "A young girl must traverse the multiverse to save her quirky brother and father.", votes: "two"})
+    assert fake_book_nonnum_vote.invalid?
+  end
+
+  test "Correctly retrieves the attributes of a book" do
+      assert_equal books(:holes).name, "Holes"
+      assert_equal books(:holes).author, "Louis Sachar"
+      assert_equal books(:holes).description, "Deviant youth dig holes in the desert for a lipstick-loving cowgirl"
+      assert_equal books(:holes).votes, 2
+  end
+    
 end
