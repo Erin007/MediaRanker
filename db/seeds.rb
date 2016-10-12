91663df9d48aee1b5397c82d74523290c2f32b72
Erin Bond <erin@erinebond.com>
2016-10-11 18:54:59 -0700

    Album seed data in albums.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,9 @@ CSV.foreach('seeds_csvs/movies.csv', :headers => true) do |csv_obj|
   Movie.create( name: csv_obj['name'], director: csv_obj['director'], description: csv_obj['description'], votes: csv_obj['votes'].to_i)
 end
 
-
+CSV.foreach('seeds_csvs/albums.csv', :headers => true) do |csv_obj|
+  Album.create( name: csv_obj['name'], artist: csv_obj['artist'], description: csv_obj['description'], votes: csv_obj['votes'].to_i)
+end
 
 
 
