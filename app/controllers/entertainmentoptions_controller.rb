ceb882aa04235d6257c72f864ac1c2fe52b290a2
Erin Bond <erin@erinebond.com>
2016-10-13 15:07:05 -0700

    Show page can display details for the selected media

--- app/controllers/entertainmentoptions_controller.rb
+++ app/controllers/entertainmentoptions_controller.rb
@@ -1,12 +1,15 @@
 class EntertainmentoptionsController < ApplicationController
   def media
     @entertainmentoption ||= Entertainmentoption.find(params[:id].to_i)
+    @book = Entertainmentoption.find(params[:id].to_i)
+    @album = Entertainmentoption.find(params[:id].to_i)
+    @movie = Entertainmentoption.find(params[:id].to_i)
   end
 
   def index
     @books = Entertainmentoption.where(mediatype: 'book').order(:votes).reverse_order
     @movies = Entertainmentoption.where(mediatype: 'movie').order(:votes).reverse_order
-    @albums = Entertainmentoption.where(mediatype: 'movie').order(:votes).reverse_order
+    @albums = Entertainmentoption.where(mediatype: 'album').order(:votes).reverse_order
   end
 
   def new
@@ -14,7 +17,22 @@ class EntertainmentoptionsController < ApplicationController
   end
 
   def show
-    media
+    @entertainmentoptions = Entertainmentoption.all
+    @book = nil
+    @album = nil
+    @movie = nil
+
+    @entertainmentoptions.each do |media|
+      number = params[:id].to_i
+      if media[:id] == number && media[:mediatype] == "book"
+        @book = media
+      elsif media[:id] == number && media[:mediatype] == "album"
+        @album = media
+      elsif media[:id] == number && media[:mediatype] == "movie"
+        @movie = media
+      end
+    end
+
   end
 
   def edit
