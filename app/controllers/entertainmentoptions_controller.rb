1aa2aa57ec86c0d5c3224c836b3d6974b0a14623
Erin Bond <erin@erinebond.com>
2016-10-13 12:18:01 -0700

    Controller methods

--- app/controllers/entertainmentoptions_controller.rb
+++ app/controllers/entertainmentoptions_controller.rb
@@ -1,25 +1,56 @@
 class EntertainmentoptionsController < ApplicationController
+  def media
+    @entertainmentoption ||= Entertainmentoption.find(params[:id].to_i)
+  end
+
   def index
+    @books = Entertainmentoption.where(mediatype: 'book').order(:votes).reverse_order
+    @movies = Entertainmentoption.where(mediatype: 'movie').order(:votes).reverse_order
+    @albums = Entertainmentoption.where(mediatype: 'movie').order(:votes).reverse_order
   end
 
   def new
+      @entertainmentoption = Entertainmentoption.new
   end
 
   def show
+    media
   end
 
   def edit
+    media
   end
 
   def update
+    media.update_attributes(media_params)
+    redirect_to root
   end
 
   def create
+    @entertainmentoption = Entertainmentoption.new(
+                     mediatype:params[:entertainmentoption][:mediatype]
+                     name:params[:entertainmentoption][:name],
+                     author:params[:entertainmentoption][:author],
+                     artist:params[:entertainmentoption][:artist],
+                     director:params[:entertainmentoption][:director]
+                     description:params[:entertainmentoption][:description],
+                     votes: 0)
+   @entertainmentoption.save
+   redirect_to :entertainmentoption
   end
 
   def destroy
+    media.destroy
+    redirect_to :entertainmentoption #change this
   end
 
   def up_vote
+    media.update_attribute(:votes, (media.votes + 1))
+    redirect_to(request.referer)
   end
+
+private
+     def media_params
+       params.require(:entertainmentoption).permit( :mediatype, :name, :author, :artist, :director, :description, :votes)
+     end
 end
