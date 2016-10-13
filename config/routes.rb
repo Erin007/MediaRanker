2c76be76921c843c7cd36b7716b4e2bfe1455a44
Erin Bond <erin@erinebond.com>
2016-10-13 15:15:46 -0700

    Trying to make the route for up
    vote work

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ root to: 'entertainmentoptions#index'
   resources :entertainmentoptions
 end
 
+resources :entertainmentoptions do
+  member do
+    post 'up_vote'
+  end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
