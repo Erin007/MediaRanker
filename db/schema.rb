a44dadb2b3259a0108d75a0dc91537c762c2ee2a
Erin Bond <erin@erinebond.com>
2016-10-14 14:07:57 -0700

    Added lion king image for testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011195836) do
+ActiveRecord::Schema.define(version: 20161014203310) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20161011195836) do
     t.string   "director"
     t.text     "description"
     t.integer  "votes"
+    t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ CSV.foreach('seeds_csvs/books.csv', :headers => true) do |csv_obj|
 end
 
 CSV.foreach('seeds_csvs/movies.csv', :headers => true) do |csv_obj|
-  Movie.create( name: csv_obj['name'], director: csv_obj['director'], description: csv_obj['description'], votes: csv_obj['votes'].to_i)
+  Movie.create( name: csv_obj['name'], director: csv_obj['director'], description: csv_obj['description'], votes: csv_obj['votes'].to_i, image: csv_obj['image'])
 end
 
 CSV.foreach('seeds_csvs/albums.csv', :headers => true) do |csv_obj|
